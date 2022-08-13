8f441f897530059b15c8a96ef65fcd668ea1331c
Jomarcelino <101756056+Jomarcelino@users.noreply.github.com>
2022-08-12 22:08:15 -0300

    Add files via upload

---
+++ livros.php
@@ -0,0 +1,17 @@
+<?php
+class livros_ 
+{
+            //ATIVIDADE
+    
+    //1º atributos (private)
+        //livre
+    //2º construct()
+        //vazio
+    //3º metodo magicos , public
+    //colocar o valor do livro e depois dar desconto
+        //livres
+
+    //4º tres objetos/ instancia 
+}
+
+?>
